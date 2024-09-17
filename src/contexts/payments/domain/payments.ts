5811fe38330572a9f8b8ff3c9406120f677b4869
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-17 19:54:58 -0300

    feat: Add Payment feature to domain layer

--- src/contexts/payments/domain/payments.ts
+++ src/contexts/payments/domain/payments.ts
@@ -1,19 +1,30 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export interface PrimitivePayment {
   id: string;
   amount: number;
   customerId: string;
 }
 
-export class Payments {
-  constructor(private atrbutes: PrimitivePayment[]) {}
+export class Payment {
+  constructor(private attributes: PrimitivePayment) {}
 
-  static create(createPayment: { amont: number; customerId: string }) {
-    return new Payments([
-      {
-        id: '123',
-        amount: createPayment.amont,
-        customerId: createPayment.customerId,
-      },
-    ]);
+  static create(createPayment: {
+    amount: number;
+    customerId: string;
+  }): Payment {
+    return new Payment({
+      id: uuidv4(),
+      amount: createPayment.amount,
+      customerId: createPayment.customerId,
+    });
+  }
+
+  toPrimitives(): PrimitivePayment {
+    return {
+      id: this.attributes.id,
+      amount: this.attributes.amount,
+      customerId: this.attributes.customerId,
+    };
   }
 }
