e1c8eb7052ccc11a8671587de4d9c4a96f7e36da
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-17 20:01:16 -0300

    feat: Add Payment repository to domain layer
    - Added `payment.repository.ts` in `src/contexts/payments/domain`.- Created abstract class `PaymentRepository` with methods `save` and `findById` to interact with the `Payment` entity in the payments domain.

---
+++ src/contexts/payments/domain/payment.repository.ts
@@ -0,0 +1,6 @@
+import { Payment } from '@/contexts/payments/domain/payment.entity';
+
+export abstract class PaymentRepository {
+  abstract save(payment: Payment): Promise<void>;
+  abstract findById(id: string): Promise<Payment | null>;
+}
